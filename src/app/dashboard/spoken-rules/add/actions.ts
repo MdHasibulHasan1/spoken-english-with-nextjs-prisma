6fdd2538471befab2888d008cebfed11feef5517
hasib1511 <hasib7143@gmail.com>
2024-10-21 15:15:43 +0600

    ffdss

--- src/app/dashboard/spoken-rules/add/actions.ts
+++ src/app/dashboard/spoken-rules/add/actions.ts
@@ -5,34 +5,31 @@ import { Rule } from "@/types/types";
 
 export async function addRule(formData: Rule) {
   // const session = await getServerSession(authOptions);
-
   /*  if (!session) {
     redirect("/api/auth/signin?callbackUrl=/add-product");
   }
- */
-  const { category, structure, note, explanation, examples, userId } = formData;
-  console.log(category, structure, note, examples);
-  if (!structure || !Array.isArray(examples) || !category) {
-    const response: ApiResponse = {
-      success: false,
-      message: "Missing required fields(structure, examples, category).",
-    };
-    return response;
-  }
-
-  try {
-    const createdRule = await prisma.spokenRule.create({
-      data: { structure, note, explanation, examples, category, userId },
-    });
-    const successResponse: ApiResponse = {
-      success: true,
-      message: "Your rule is added successfully",
-      payload: createdRule,
-    };
-    return successResponse;
-  } catch (error) {
-    console.log(error);
-  }
-
+//  */
+  //   const { category, structure, note, explanation, examples, userId } = formData;
+  //   console.log(category, structure, note, examples);
+  //   if (!structure || !Array.isArray(examples) || !category) {
+  //     const response: ApiResponse = {
+  //       success: false,
+  //       message: "Missing required fields(structure, examples, category).",
+  //     };
+  //     return response;
+  //   }
+  //   try {
+  //     const createdRule = await prisma.spokenRule.create({
+  //       data: { structure, note, explanation, examples, category, userId },
+  //     });
+  //     const successResponse: ApiResponse = {
+  //       success: true,
+  //       message: "Your rule is added successfully",
+  //       payload: createdRule,
+  //     };
+  //     return successResponse;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
   // redirect("/");
 }
